7267b217a595268122604286131736ff863529ad
kxznn <kauaroaguiar16@gmail.com>
2025-09-15 22:14:50 -0300

    Exercicio
    Exercicios com Array

--- aula06/exercicio/Ex1.js
+++ aula06/exercicio/Ex1.js
@@ -1,1 +1,15 @@
-    
+// EXERCÍCIO 01
+
+let Games = ['Fortnite', 'Hollow Knight', 'Minecraft', 'GTA V', 'Call of Duty'];
+console.log(Games);
+
+Games[0] = 'Roblox';
+console.log('Array alterado:', Games);
+
+console.log("-------------------------------------------------------------------------------------");
+
+// EXERCÍCIO 02
+
+let meuArray = [];
+meuArray.push(10, 20, 30);
+console.log(meuArray);
